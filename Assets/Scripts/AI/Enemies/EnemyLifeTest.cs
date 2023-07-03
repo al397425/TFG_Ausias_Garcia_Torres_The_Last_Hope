a7ae4ce620c5a6e9c067a9194b00c40db5121935
ausiasxp <90216787+al397425@users.noreply.github.com>
2023-07-03 18:38:22 +0200

    Enemy life and Colour when damage

--- Assets/Scripts/AI/Enemies/EnemyLifeTest.cs
+++ Assets/Scripts/AI/Enemies/EnemyLifeTest.cs
@@ -5,11 +5,15 @@ using UnityEngine;
 public class EnemyLifeTest : MonoBehaviour
 {
     [SerializeField] private int life = 1;
-    
+    private bool candamage = true;
+    private GameObject turtleShell;
+    Renderer RendererEnemy;
     // Start is called before the first frame update
     void Start()
     {
-        
+        turtleShell = transform.GetChild(1).gameObject;
+        print(turtleShell);
+        RendererEnemy= turtleShell.GetComponent<Renderer>();
     }
 
     // Update is called once per frame
@@ -21,10 +25,27 @@ public class EnemyLifeTest : MonoBehaviour
         
     }
     private void OnCollisionEnter(Collision collision) {
-        if (collision.gameObject.tag == "SwordCollider")
+        if (collision.gameObject.tag == "SwordCollider" && candamage == true)
         {
-            life--;
+            StartCoroutine(Damage());
+            StartCoroutine(Renderanimation());
             
         }
+    }
+    private IEnumerator Damage()
+    {
+        life--;
+        candamage = false;
+        print(life +"me queda vida");
+        yield return new WaitForSeconds(0.5f);
+        candamage = true;
+        
+        
+    }
+    private IEnumerator Renderanimation()
+    {
+    RendererEnemy.material.SetColor("_Color", Color.red);
+    yield return new WaitForSeconds(0.3f);
+    RendererEnemy.material.SetColor("_Color", Color.white);
     }
 }
